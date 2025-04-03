87d421ba75b610b9a212cbdae3bee43877aa0348
Tracey Jaquith <tracey@archive.org>
2025-04-02 22:33:05 -0700

    url updates

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
 
 // you can toggle these for local dev, with a pulled down version of this file
-import 'https://blogtini.com/js/blogtini.js'
+import 'https://blogtini.com/index.js'
 // import './blogtini.js'
